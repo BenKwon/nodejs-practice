f6645b90ead0a01dba0f48b017db8a2a4a44300b
BenKwon <kwon5891@gmail.com>
2021-09-26 22:50:09 +0900

    Update gitignore

--- RSC예시/node/router/comment-router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const CommentController = require("../controller/comment-controller");
-router.post("/", CommentController.postComment);
-router.get("/", CommentController.getComments);
-
-module.exports = router;
